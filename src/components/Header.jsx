2fb7fa52df5ec2a26fa1564bfbd01d5c6c9a97f3
Roman <romanxs610@gmail.com>
2022-08-04 20:49:33 +0300

    tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return  <header className='header'>
+                <img src="https://www.visualpro360.it/wp-content/uploads/2016/03/DJI1.png"/>
+            </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Profile = () => {
+    return  <div className='content'>
+              <img src="https://avatars.mds.yandex.net/get-zen_doc/3957666/pub_5fb385ea268198734de4bb7a_5fb38690268198734de5ece0/scale_1200"/>
+              <div>
+              ava + discription
+              </div>
+              <div>
+              My posts
+              </div>
+              <div>
+              New Posts
+              </div>
+              <div>
+              Posts 1
+              </div>
+              <div>
+              Posts 2
+              </div>
+            </div>
+}
+
+export default Profile;
