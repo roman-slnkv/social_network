fe6cefbdcb8f22109ef6c1954081a20449703111
Roman <romanxs610@gmail.com>
2022-08-09 14:05:00 +0300

    создание диалогов - компонент  Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogs__items}>
+
+                <div className={s.dialog + ' ' + s.active}>Николай</div>
+                <div className={s.dialog}>Александр</div>
+                <div className={s.dialog}>Света</div>
+                <div className={s.dialog}>Елена</div>
+                <div className={s.dialog}>Софья</div>
+                <div className={s.dialog}>Роман</div>
+
+            </div>
+
+            <div className={s.message}>
+
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How is your</div>
+                <div className={s.dialog}>Yo</div>
+
+            </div>
+
         </div>
     )
 }
